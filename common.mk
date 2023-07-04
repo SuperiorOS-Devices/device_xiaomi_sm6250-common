094ef5cdde7565e5d2d5a506e80f98dbefba8be2
userariii <abhattacharjee717@gmail.com>
2023-07-04 05:12:57 +0000

    sm6250-common: if statement for miuiCamera

--- common.mk
+++ common.mk
@@ -321,7 +321,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
 # MiuiCamera
+ifeq ($(SHIP_MIUICAM),true)
 $(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
+endif
 
 # Network
 PRODUCT_COPY_FILES += \
